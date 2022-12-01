fadb41becb3cbf7e7d0954f07ec385515c6d9239
CosmosKiller <mnsamur2014@gmail.com>
2022-12-01 18:32:13 -0300

    flash memory usage optimizations

--- automatedIrrigationSystem/src/main.cpp
+++ automatedIrrigationSystem/src/main.cpp
@@ -20,13 +20,12 @@ snrThrPin_t mySensors[]{
     {30, SNRWL_PIN}};
 
 CosmosPump_t myPump = {
-    .pump = devices[0],
+    .pump = &devices[0],
     .ledPins = devices[3].pin,
     .snrWl = mySensors[1],
-    .snrTh = mySensors[0]
-};
+    .snrTh = mySensors[0]};
 
-snrData_t sensorData = {};
+snrData_t sensorData = {0, 0, 0, 0};
 
 // Global variables
 long lastMsg = 0;
@@ -40,21 +39,29 @@ void codeForTask1(void *parameter)
   for (;;)
   {
     if (devices[0].state == HIGH)
-      pumpControl(myPump, ENGAGE_RN, &sensorData);
+      pumpControl(&myPump, ENGAGE_RN, &sensorData);
     else
-      pumpControl(myPump, ENGAGE_NO, &sensorData);
+      pumpControl(&myPump, ENGAGE_NO, &sensorData);
+
+    Serial.println(devices[0].state);
 
     long now = millis();
     if (now - lastMsg > 2000)
     {
-      lastMsg = now;
-      msg = String(sensorData.wlData);
-      msg.toCharArray(toSend, 30);
-      cosmosMqttPublish(toSend, devices[2].sn, RX_CONTROL);
+      if (!isnan(sensorData.wlData))
+      {
+        lastMsg = now;
+        msg = String(sensorData.wlData);
+        msg.toCharArray(toSend, 30);
+        cosmosMqttPublish(toSend, devices[2].sn, RX_CONTROL);
+      }
 
-      msg = String(sensorData.tmData) + "," + String(sensorData.smData);
-      msg.toCharArray(toSend, 30);
-      cosmosMqttPublish(toSend, devices[1].sn, RX_CONTROL);
+      if (!isnan(sensorData.tmData) && !isnan(sensorData.hmData))
+      {
+        msg = String(sensorData.tmData) + "," + String(sensorData.hmData);
+        msg.toCharArray(toSend, 30);
+        cosmosMqttPublish(toSend, devices[1].sn, RX_CONTROL);
+      }
     }
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
@@ -88,7 +95,7 @@ void loop()
 static void myCallback(char *topic, byte *payload, unsigned int length)
 {
   if (devices[0].state == LOW)
-    pumpControl(myPump, ENGAGE_OK, &sensorData);
+    pumpControl(&myPump, ENGAGE_OK, &sensorData);
   else
   {
     // Reset pump state
